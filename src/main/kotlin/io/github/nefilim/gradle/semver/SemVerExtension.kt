4917a5185eaf50a4590d4720d3a88813f4c94022
Peter vR <peter@nefilim.org>
2022-01-03 09:30:28 -0700

    restructure plugin to be able to control version calculation time from the build script (#9)

--- src/main/kotlin/io/github/nefilim/gradle/semver/SemVerExtension.kt
+++ src/main/kotlin/io/github/nefilim/gradle/semver/SemVerExtension.kt
@@ -1,9 +1,12 @@
 package io.github.nefilim.gradle.semver
 
+import arrow.core.getOrElse
+import arrow.core.getOrHandle
 import arrow.core.toOption
 import com.javiersc.semver.Version
 import io.github.nefilim.gradle.semver.config.PluginConfig
 import io.github.nefilim.gradle.semver.config.Scope
+import io.github.nefilim.gradle.semver.config.SemVerPluginContext
 import io.github.nefilim.gradle.semver.config.Stage
 import io.github.nefilim.gradle.semver.config.possiblyPrefixedVersion
 import io.github.nefilim.gradle.semver.domain.GitRef
@@ -11,53 +14,54 @@ import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
-import org.gradle.api.provider.Provider
 import javax.inject.Inject
 
-open class SemVerExtension @Inject constructor(objects: ObjectFactory) {
-    private val verbose: Property<Boolean> = objects.property(Boolean::class.java)
-    private val tagPrefix: Property<String> = objects.property(String::class.java)
-    private val initialVersion: Property<Version> = objects.property(Version::class.java)
-    private val overrideVersion: Property<Version> = objects.property(Version::class.java)
-    private val calculatedVersionInternal: Property<Version> = objects.property(Version::class.java)
-    val calculatedVersion: Provider<Version> = calculatedVersionInternal
-    val calculatedTagName: Provider<String> = calculatedVersionInternal.flatMap { v -> tagPrefix.map { "$it$v"} }
-
-    private val main: BranchHandler = objects.newInstance(BranchHandler::class.java, objects, GitRef.MainBranch.DefaultScope, GitRef.MainBranch.DefaultStage)
-    private val develop: BranchHandler = objects.newInstance(BranchHandler::class.java, objects, GitRef.DevelopBranch.DefaultScope, GitRef.DevelopBranch.DefaultStage)
-    private val feature: BranchHandler = objects.newInstance(BranchHandler::class.java, objects, GitRef.FeatureBranch.DefaultScope, GitRef.FeatureBranch.DefaultStage)
-    private val hotfix: BranchHandler = objects.newInstance(BranchHandler::class.java, objects, GitRef.HotfixBranch.DefaultScope, GitRef.HotfixBranch.DefaultStage)
-
-    init {
-        verbose.set(true)
-        tagPrefix.set(PluginConfig.DefaultTagPrefix)
-        initialVersion.set(PluginConfig.DefaultVersion)
-        overrideVersion.set(null)
-    }
+open class SemVerExtension @Inject constructor(objects: ObjectFactory, private val project: Project) {
+    private val verbose: Property<Boolean> = objects.property(Boolean::class.java).convention(true)
+    private val tagPrefix: Property<String> = objects.property(String::class.java).convention(PluginConfig.DefaultTagPrefix)
+    private val initialVersion: Property<Version> = objects.property(Version::class.java).convention(PluginConfig.DefaultVersion)
+    private val overrideVersion: Property<Version> = objects.property(Version::class.java).convention(null)
 
     fun verbose(b: Boolean) {
         verbose.set(b)
         verbose.disallowChanges()
     }
-
     fun tagPrefix(prefix: String) {
         if (overrideVersion.orNull != null)
             throw IllegalArgumentException("cannot set the semver tagPrefix after override version has been set, the override version depends on the tagPrefix, set the tagPrefix first")
         tagPrefix.set(prefix)
         tagPrefix.disallowChanges()
     }
-
     fun initialVersion(version: String?) {
         version?.also {
             initialVersion.set(Version(it))
             initialVersion.disallowChanges()
         }
     }
-
     fun overrideVersion(version: String) {
         overrideVersion.set(possiblyPrefixedVersion(version, tagPrefix.get())) // not great, requires tagPrefix to be set first
         overrideVersion.disallowChanges()
     }
+
+    // defer version calculation since all our properties are lazy and needs to be configured first
+    fun version(): Version {
+        val config = this.buildPluginConfig()
+        val context = SemVerPluginContext(project.git, config, project)
+        context.log("semver configuration: $config")
+
+        return config.overrideVersion.getOrElse {
+            context.calculateVersionFlow().getOrHandle {
+                context.error("failed to calculate version: $it".red())
+                throw Exception("$it")
+            }
+        }
+    }
+    fun versionTagName(): String = "$tagPrefix${version()}"
+
+    private val main: BranchHandler = BranchHandler(objects, GitRef.MainBranch.DefaultScope, GitRef.MainBranch.DefaultStage)
+    private val develop: BranchHandler = BranchHandler(objects, GitRef.DevelopBranch.DefaultScope, GitRef.DevelopBranch.DefaultStage)
+    private val feature: BranchHandler = BranchHandler(objects, GitRef.FeatureBranch.DefaultScope, GitRef.FeatureBranch.DefaultStage)
+    private val hotfix: BranchHandler = BranchHandler(objects, GitRef.HotfixBranch.DefaultScope, GitRef.HotfixBranch.DefaultStage)
 
     fun main(action: Action<BranchHandler>) {
         action.execute(main)
@@ -72,17 +76,12 @@ open class SemVerExtension @Inject constructor(objects: ObjectFactory) {
         action.execute(hotfix)
     }
 
-    fun setVersion(version: Version) {
-        calculatedVersionInternal.set(version)
-        calculatedVersionInternal.disallowChanges()
-    }
-
-    fun buildPluginConfig(): PluginConfig {
+    private fun buildPluginConfig(): PluginConfig {
         return PluginConfig(
             verbose.get(),
             tagPrefix.get(),
             initialVersion.get(),
-            overrideVersion.orNull.toOption(),
+            overrideVersion.get().toOption(),
             main.scope.get(),
             main.stage.get(),
             develop.scope.get(),
--- src/main/kotlin/io/github/nefilim/gradle/semver/SemVerPlugin.kt
+++ src/main/kotlin/io/github/nefilim/gradle/semver/SemVerPlugin.kt
@@ -15,41 +15,13 @@ import org.gradle.api.Project
 
 public class SemVerPlugin: Plugin<Project> {
     override fun apply(target: Project) {
-        val semVerExtension = target.semver()
-
-        if (target.hasGit) {
-            target.afterEvaluate {
-                val config = semVerExtension.buildPluginConfig()
-                val context = SemVerPluginContext(target.git, config, target)
-                context.verbose("semver configuration: $config")
-
-                val calculatedVersion = config.overrideVersion.getOrElse {
-                    context.calculateVersionFlow().getOrHandle {
-                        context.error("failed to calculate version: $it".red())
-                        throw Exception("$it")
-                    }
-                }
-                target.version = calculatedVersion.toString()
-                semVerExtension.setVersion(calculatedVersion)
-                context.generateVersionFile()
-
-                if (target == target.rootProject)
-                    target.allprojects { it.project.version = target.version }
-
-                target.gradle.projectsEvaluated {
-                    if (target.appliedOnlyOnRootProject)
-                        context.verbose("semver: ${target.version}".purple())
-                    else
-                        context.verbose("semver for ${target.name}: ${target.version}".purple())
-                }
-            }
-        } else {
+        target.semver()
+        if (!target.hasGit)
             target.logger.warn("the current directory is not part of a git repo, cannot determine project semantic version number, please initialize a git repo with main & develop branches")
-        }
     }
 }
 
-private fun SemVerPluginContext.calculateVersionFlow(): Either<SemVerError, Version> {
+internal fun SemVerPluginContext.calculateVersionFlow(): Either<SemVerError, Version> {
     return either.eager {
         val allBranches = Either.catch { git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call().toList() }.mapLeft { SemVerError.Git(it) }.bind()
         val mainRefName = allBranches.firstOrNull { setOf(GitRef.MainBranch.RefName, GitRef.MainBranch.RemoteOriginRefName).contains(it.name) }?.name
