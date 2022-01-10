1a5883d21962fd732a4558c55841415600421a0e
Peter vR <peter@nefilim.org>
2022-01-10 15:07:07 -0700

    add generateVersionFile task (#19)

--- src/main/kotlin/io/github/nefilim/gradle/semver/Gradle.kt
+++ src/main/kotlin/io/github/nefilim/gradle/semver/Gradle.kt
@@ -2,31 +2,12 @@ package io.github.nefilim.gradle.semver
 
 import io.github.nefilim.gradle.semver.config.SemVerPluginContext
 import org.gradle.api.Project
-import java.io.File
 
 internal val Project.hasSemVerPlugin: Boolean
     get() = pluginManager.hasPlugin("io.github.nefilim.gradle.semver-plugin")
 
 internal val Project.appliedOnlyOnRootProject: Boolean
     get() = rootProject.hasSemVerPlugin && rootProject.subprojects.none(Project::hasSemVerPlugin)
-
-internal fun SemVerPluginContext.generateVersionFile() {
-    with (project) {
-        File("$buildDir/semver/version.txt").apply {
-            parentFile.mkdirs()
-            createNewFile()
-            writeText(
-                """
-                   |$version
-                   |${config.tagPrefix}$version
-                   |
-                """.trimMargin()
-            )
-        }
-    }
-}
-
-internal fun Project.semverMessage(message: Any) = logger.lifecycle("$message")
 
 internal fun SemVerPluginContext.log(message: String) {
     project.logger.lifecycle(message)
