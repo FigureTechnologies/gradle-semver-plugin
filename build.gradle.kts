9a0ab1594eca232aabab6ab78ed38581d575ad6e
hatz <ahatzenbuhler@figure.com>
2022-06-01 08:52:54 -0500

    try without maven-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ import org.jetbrains.kotlin.config.KotlinCompilerVersion.VERSION as KOTLIN_VERSI
 plugins {
     `java-gradle-plugin`
     signing
-    `maven-publish`
     alias(libs.plugins.github.release)
     alias(libs.plugins.gradle.plugin.publish)
     alias(libs.plugins.kotlin.jvm)
