0ce99d94e53a033f5f83ca5a72c46ce6f88c7182
Tyler Crawford <tcrawford@figure.com>
2023-01-19 16:05:11 -0500

    Update language and api version for kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 import java.util.Calendar
 import org.jetbrains.kotlin.config.KotlinCompilerVersion.VERSION as KOTLIN_VERSION
 
@@ -75,8 +77,8 @@ kotlin {
                 freeCompilerArgs =
                     freeCompilerArgs + listOf("-version", "-Xjsr305=strict", "-opt-in=kotlin.RequiresOptIn")
                 jvmTarget = "11"
-                languageVersion = "1.7"
-                apiVersion = "1.7"
+                languageVersion = "1.8"
+                apiVersion = "1.8"
                 verbose = true
             }
         }
@@ -99,8 +101,8 @@ tasks.withType<Test>().configureEach {
         showStandardStreams = true
         showCauses = true
         showStackTraces = true
-        events(*org.gradle.api.tasks.testing.logging.TestLogEvent.values())
-        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        events(*TestLogEvent.values())
+        exceptionFormat = TestExceptionFormat.FULL
     }
 }
 
