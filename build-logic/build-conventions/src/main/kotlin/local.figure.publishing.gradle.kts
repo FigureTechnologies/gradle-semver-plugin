a1c1985ba0ca77e6566bf1fcd298adad2ac69c07
hatz <ahatzenbuhler@figure.com>
2023-02-15 12:04:28 -0600

    Add pom metadata

--- build-logic/build-conventions/src/main/kotlin/local.figure.publishing.gradle.kts
+++ build-logic/build-conventions/src/main/kotlin/local.figure.publishing.gradle.kts
@@ -12,7 +12,7 @@ group = "com.figure.gradle"
 description = "Gradle Plugin for Automated Semantic Versioning"
 
 inner class ProjectInfo {
-    val longName = "Gradle Semver Plugin"
+    val name = "Gradle Semver Plugin"
     val description = "Gradle Plugin for Automated Semantic Versioning"
     val pluginImplementationClass = "$group.semver.SemverPlugin"
     val tags = listOf("semver", "git semver", "versioning")
@@ -28,7 +28,7 @@ gradlePlugin {
     plugins {
         create(project.name) {
             id = "$group.${project.name}"
-            displayName = info.longName
+            displayName = info.name
             description = info.description
             tags.set(info.tags)
             implementationClass = info.pluginImplementationClass
@@ -36,60 +36,47 @@ gradlePlugin {
     }
 }
 
-//publishing {
-//    repositories {
-//        sonatype {
-//            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-//            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-//            username.set(findProject("ossrhUsername")?.toString() ?: System.getenv("OSSRH_USERNAME"))
-//            password.set(findProject("ossrhPassword")?.toString() ?: System.getenv("OSSRH_PASSWORD"))
-//            stagingProfileId.set("858b6e4de4734a") // tech.figure staging profile id
-//        }
-//    }
-//     publications {
-//         create<MavenPublication>("mavenJava") {
-//             pom {
-//                 // This line is what includes the java{} block, aka javadocs and sources
-//                 from(components["java"])
-//
-//                 name.set(info.longName)
-//                 description.set(info.description)
-//                 url.set(info.website)
-//                 licenses {
-//                     license {
-//                         name.set("The Apache License, Version 2.0")
-//                         url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//                     }
-//                 }
-//                 developers {
-//                     developer {
-//                         id.set("ahatzz11")
-//                         name.set("Alex Hatzenbuhler")
-//                         email.set("ahatzenbuhler@figure.com")
-//                     }
-//                     developer {
-//                         id.set("tcrawford")
-//                         name.set("Tyler Crawford")
-//                         email.set("tcrawford@figure.com")
-//                     }
-//                     developer {
-//                         id.set("jonasg13")
-//                         name.set("Jonas Gorauskas")
-//                         email.set("jgorauskas@figure.com")
-//                     }
-//                 }
-//                 scm {
-//                     connection.set("scm:git:git://github.com/FigureTechnologies/gradle-semver-plugin.git")
-//                     developerConnection.set("scm:git:ssh://github.com/FigureTechnologies/gradle-semver-plugin.git")
-//                     url.set(info.website)
-//                 }
-//             }
-//         }
-//     }
-//}
-
-//if (!System.getenv("DISABLE_SIGNING").toBoolean()) {
-//    configure<SigningExtension> {
-//        sign(publications["maven"])
-//    }
-//}
+afterEvaluate {
+    publishing {
+        publications.filterIsInstance<MavenPublication>().forEach {
+            it.pom {
+                name.set(info.name)
+                description.set(info.description)
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("figure-oss")
+                        name.set("Figure OSS Engineers")
+                        email.set("oss@figure.com")
+                    }
+                    developer {
+                        id.set("ahatzz11")
+                        name.set("Alex Hatzenbuhler")
+                        email.set("ahatzenbuhler@figure.com")
+                    }
+                    developer {
+                        id.set("tcrawford")
+                        name.set("Tyler Crawford")
+                        email.set("tcrawford@figure.com")
+                    }
+                    developer {
+                        id.set("jonasg13")
+                        name.set("Jonas Gorauskas")
+                        email.set("jgorauskas@figure.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/FigureTechnologies/gradle-semver-plugin.git")
+                    developerConnection.set("scm:git:ssh://github.com/FigureTechnologies/gradle-semver-plugin.git")
+                    url.set(info.website)
+                }
+            }
+        }
+    }
+}
