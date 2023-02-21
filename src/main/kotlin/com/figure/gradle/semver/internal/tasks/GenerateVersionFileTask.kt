3bb04a13aef5820cb3373f82f08c3cb76c90e3b5
hatz <ahatzenbuhler@figure.com>
2023-02-21 17:03:18 -0600

    Add cacheable task annotation

--- src/main/kotlin/com/figure/gradle/semver/internal/tasks/GenerateVersionFileTask.kt
+++ src/main/kotlin/com/figure/gradle/semver/internal/tasks/GenerateVersionFileTask.kt
@@ -10,12 +10,13 @@ package com.figure.gradle.semver.internal.tasks
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import org.gradle.work.DisableCachingByDefault
 
-//@DisableCachingByDefault(because = "Not worth caching")
+@CacheableTask
 internal abstract class GenerateVersionFileTask : DefaultTask() {
 
     @get:OutputFile
