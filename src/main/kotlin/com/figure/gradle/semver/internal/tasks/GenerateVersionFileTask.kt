28ff6a525856bef8f4b281008782ba7ec3806400
Tyler Crawford <tcrawford@figure.com>
2023-02-21 11:14:23 -0500

    Disable configuration for all tasks for now

--- src/main/kotlin/com/figure/gradle/semver/internal/tasks/GenerateVersionFileTask.kt
+++ src/main/kotlin/com/figure/gradle/semver/internal/tasks/GenerateVersionFileTask.kt
@@ -10,15 +10,15 @@ package com.figure.gradle.semver.internal.tasks
 import com.figure.gradle.semver.internal.semverDebug
 import org.gradle.api.DefaultTask
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
+import org.gradle.work.DisableCachingByDefault
 import java.io.File
 
-@CacheableTask
+@DisableCachingByDefault(because = "Not worth caching")
 internal abstract class GenerateVersionFileTask : DefaultTask() {
     @get:InputDirectory
     @get:PathSensitive(PathSensitivity.ABSOLUTE)
