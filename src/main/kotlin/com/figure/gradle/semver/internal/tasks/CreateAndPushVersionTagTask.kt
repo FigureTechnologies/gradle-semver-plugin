d1b46a5aba0b8b6502da1180ed7bde55dd9570d5
Tyler Crawford <tcrawford@figure.com>
2023-03-27 10:53:12 -0400

    Try internal

--- src/main/kotlin/com/figure/gradle/semver/internal/tasks/CreateAndPushVersionTagTask.kt
+++ src/main/kotlin/com/figure/gradle/semver/internal/tasks/CreateAndPushVersionTagTask.kt
@@ -11,7 +11,7 @@ import com.figure.gradle.semver.internal.semverLifecycle
 import org.eclipse.jgit.api.Git
 import org.gradle.api.DefaultTask
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.UntrackedTask
 
@@ -22,10 +22,10 @@ import org.gradle.api.tasks.UntrackedTask
     because = "Git already takes care of keeping the state. There are issues with using Gradle caching and this task."
 )
 abstract class CreateAndPushVersionTagTask : DefaultTask() {
-    @get:Input
+    @get:Internal
     abstract val versionTagName: Property<String>
 
-    @get:Input
+    @get:Internal
     abstract val git: Property<Git>
 
     @TaskAction
